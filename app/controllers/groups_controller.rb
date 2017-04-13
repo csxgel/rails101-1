90897402999141d77173e4fdc37040ffc1a335e9
csxgel <DonaldZhang@zhangsixudeMacBook-Pro.local>
2017-04-13 16:05:55 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
@@ -23,7 +23,7 @@ class GroupsController < ApplicationController
     @group.user = current_user
 
     if @group.save
-
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -46,20 +46,45 @@ class GroupsController < ApplicationController
     redirect_to groups_path
   end
 
+  def join
+   @group = Group.find(params[:id])
 
-  private
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本讨论版成功!"
+    else
+      flash[:warning] = "你已经是本讨论版成员了！"
+    end
 
-  def group_params
-    params.require(:group).permit(:title, :description)
+    redirect_to group_path(@group)
   end
 
-  def find_group_and_check_permission
+  def quit
     @group = Group.find(params[:id])
 
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本讨论版！"
+    else
+      flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
     end
+
+    redirect_to group_path(@group)
   end
+
+   private
+
+   def group_params
+     params.require(:group).permit(:title, :description)
+   end
+
+   def find_group_and_check_permission
+     @group = Group.find(params[:id])
+
+     if current_user != @group.user
+       redirect_to root_path, alert: "You have no permission."
+     end
+   end
 
 
 end
